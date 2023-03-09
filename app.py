22dfa59754345444e6d253c7ebc92f81423a7fd5
Kayla <k_meijer_10@yahoo.com>
2023-03-09 18:45:21 -0500

    Added Model method, Added Schema, Added Resources

--- app.py
+++ app.py
@@ -23,15 +23,40 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(255), nullable=False)
+    model = db.Column(db.String(255),nullable=False)
+    year = db.Column(db.Integer)
 
+    def __repr__(self):
+        return f'{self.year} {self.make} {self.model}'
 
 
 # Schemas
+class CarSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "make", "model", "year")
 
-
+car_schema = CarSchema()
+cars_schema = CarSchema(many=True)
 
 # Resources
-
-
+class CarListResource(Resource):
+    def get(self):
+        all_cars = Car.query.all()
+        return cars_schema.dump(all_cars)
+    
+    def post(self):
+        print(request)
+        new_car = Car(
+            make=request.json['make'],
+            model=request.json['model'],
+            year=request.json['year']
+        )
+        db.session.add(new_car)
+        db.session.commit()
+        return car_schema.dump(new_car), 201
 
 # Routes
+api.add_resource(CarListResource, '/api/cars')
